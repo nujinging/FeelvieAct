cdfcd3d88b033494123b81dc2442672a1893433c
nujiging <iikasam@naver.com>
2023-09-04 16:48:13 +0900

    간략하게 수정ㅈ우

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import List from "./components/List";
 
 
 function App() {
-    const [playing, setPlaying] = useState([]);
-    const [popularList, setPopularList] = useState([]);
-    const [dayList, setDaylist] = useState([]);
-    const [weekList, setWeeklist] = useState([]);
 
+    const [lists, setLists] = useState({
+        playing : [],
+        popular : [],
+        day : [],
+        week : []
+    })
     useEffect(() => {
         async function Api() {
             const play = await movieApi.nowPlaying('')
@@ -19,10 +21,12 @@ function App() {
             const week = await movieApi.today('week')
 
 
-            setPlaying(play.data.results)
-            setPopularList(popular.data.results)
-            setDaylist(day.data.results)
-            setWeeklist(week.data.results)
+            setLists({
+                playing : play.data.results,
+                popular : popular.data.results,
+                day : day.data.results,
+                week : week.data.results
+            })
         }
         Api();
     }, []);
@@ -35,23 +39,32 @@ function App() {
                   <div className="title">
                       <h2>지금 상영중이에요!</h2>
                   </div>
-                  <List list={playing}></List>
+                  {Object.entries(lists).map(([title, list]) => (
+                      <List list={list}></List>
+                  ))}
 
-                  <div className="title">
-                      <h2>가장 인기있는 컨텐츠</h2>
-                  </div>
-                  <List list={popularList}></List>
+                  {/*<div className="title" key={title}>*/}
+                  {/*    <h2>{title === 'playing' ? '지금 상영중이에요!' : title === 'popular' ? '가장 인기있는 컨텐츠' : title === 'day' ? '오늘 가장 많이 찾아 본 컨텐츠' : '이번 주 가장 많이 찾아 본 컨텐츠'}</h2>*/}
+                  {/*    <List list={list} />*/}
+                  {/*</div>*/}
+
+                  {/*<List list={playing}></List>*/}
+
+                  {/*<div className="title">*/}
+                  {/*    <h2>가장 인기있는 컨텐츠</h2>*/}
+                  {/*</div>*/}
+                  {/*<List list={popularList}></List>*/}
 
 
-                  <div className="title">
-                      <h2>오늘 가장 많이 찾아 본 컨텐츠</h2>
-                  </div>
-                  <List list={dayList}></List>
+                  {/*<div className="title">*/}
+                  {/*    <h2>오늘 가장 많이 찾아 본 컨텐츠</h2>*/}
+                  {/*</div>*/}
+                  {/*<List list={dayList}></List>*/}
 
-                  <div className="title">
-                      <h2>이번 주 가장 많이 찾아 본 컨텐츠</h2>
-                  </div>
-                  <List list={weekList}></List>
+                  {/*<div className="title">*/}
+                  {/*    <h2>이번 주 가장 많이 찾아 본 컨텐츠</h2>*/}
+                  {/*</div>*/}
+                  {/*<List list={weekList}></List>*/}
 
               </div>
           </div>
