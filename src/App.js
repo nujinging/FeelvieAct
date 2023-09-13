1a3379bf24cfb193cb0227bb4bdf699719ff3172
nujiging <iikasam@naver.com>
2023-09-13 13:08:02 +0900

    재정리 + css 추가

--- src/App.js
+++ src/App.js
@@ -1,65 +1,18 @@
 
 import './App.scss';
-import { movieApi } from "./util/movieApi";
-import { useState, useEffect } from 'react';
-import List from "./components/List";
-import HomeSlide from "./components/HomeSlide";
+import { Route, Routes } from 'react-router-dom';
+import Main from "./components/Main";
+import ItemDetail from "./components/ItemDetail";
+
 
 
 function App() {
 
-    const titles = {
-        playing: '지금 상영중이에요!',
-        popular: '가장 인기있는 영화',
-        day: '오늘 하루 가장 많이 본 영화',
-        week: '이번 주 가장 많이 본 영화',
-    }
-
-    const [lists, setLists] = useState({
-        playing : [],
-        popular : [],
-        day : [],
-        week : [],
-        main: []
-    })
-    useEffect(() => {
-        async function Api() {
-            const play = await movieApi.nowPlaying('')
-            const popular = await movieApi.popular('')
-            const day = await movieApi.today('day')
-            const week = await movieApi.today('week')
-
-
-            setLists({
-                playing : play.data.results,
-                popular : popular.data.results,
-                day : day.data.results,
-                week : week.data.results
-            })
-        }
-        Api();
-    }, []);
-
   return (
-      <div className="App">
-          <div className="container">
-              <HomeSlide lists={lists.popular}></HomeSlide>
-              <div className="item_container">
-                  {Object.entries(titles).reduce((acc, [key, title]) => {
-                      const list = lists[key];
-                      acc.push(
-                          <div className="title" key={key}>
-                              <h2>{title}</h2>
-                          </div>
-                      );
-                      acc.push(
-                          <List key={`list_${key}`} list={list}></List>
-                      );
-                      return acc;
-                  }, [])}
-              </div>
-          </div>
-      </div>
+      <Routes>
+          <Route path='/' element={<Main />} ></Route>
+          <Route path='/detail' element={<ItemDetail />} ></Route>
+      </Routes>
   );
 }
 
