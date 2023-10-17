0a66f56a504967f7511061e25a0f18868e268b26
nujinging <nujinging@naver.com>
2023-10-17 18:11:44 +0900

    video제대로 꽂기

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,9 +1,9 @@
-import { Swiper, SwiperSlide } from "swiper/react";
+import {Swiper, SwiperSlide} from "swiper/react";
 import "swiper/css";
 import './../App.scss';
 import {movieApi} from "../util/movieApi";
 import {useEffect, useState, useRef} from "react";
-import { Link, useParams, useNavigate } from "react-router-dom";
+import {Link, useParams, useNavigate} from "react-router-dom";
 import List from "./List";
 
 export default function ItemDetail() {
@@ -18,7 +18,7 @@ export default function ItemDetail() {
     const [imagesUrl, setImagesUrl] = useState();
     const [videoUrl, setVideoUrl] = useState();
 
-    const [mediaType, setMediaType] = useState('backdrops');
+    const [mediaType, setMediaType] = useState('video');
     const [overviewMore, setOverviewMore] = useState(false);
     const textContainerRef = useRef(null);
     const [isOverflowed, setIsOverflowed] = useState(false);
@@ -33,9 +33,8 @@ export default function ItemDetail() {
     const [youtubeUrl, setYoutubeUrl] = useState()
 
 
-
     const [videoLink, setVideoLink] = useState();
-    const [videoSize, setVideoSize] = useState({ width: 0, height: 0 });
+    const [videoSize, setVideoSize] = useState({width: 0, height: 0});
 
     // 미디어 - 배경,포스터
     const mediaTab = (type) => {
@@ -50,7 +49,7 @@ export default function ItemDetail() {
 
         const video_width = item.width;
         const video_height = item.height;
-        setVideoSize({ width: video_width, height: video_height });
+        setVideoSize({width: video_width, height: video_height});
     };
     const imgModalClose = () => {
         setImgModal(!imgModal);
@@ -69,8 +68,6 @@ export default function ItemDetail() {
     };
 
 
-
-
     // 영화 상세설명
     useEffect(() => {
         const textContainer = textContainerRef.current;
@@ -85,79 +82,80 @@ export default function ItemDetail() {
     };
 
     // 시즌 에피소드 5개 보여주기
-    const seasonList = seasonUrl?.episodes.slice(0,5);
-
+    const seasonList = seasonUrl?.episodes.slice(0, 5);
 
 
     useEffect(() => {
         async function Api() {
-           try {
-               window.scrollTo(0, 0);
-               const detail = await movieApi.detail(params.type, params.id);
-               const credits = await movieApi.credits(params.type, params.id);
-               const similar = await movieApi.similar(params.type, params.id);
-               const social = await movieApi.social(params.type, params.id);
+            try {
+                window.scrollTo(0, 0);
+                const detail = await movieApi.detail(params.type, params.id);
+                const credits = await movieApi.credits(params.type, params.id);
+                const similar = await movieApi.similar(params.type, params.id);
+                const social = await movieApi.social(params.type, params.id);
 
-               const textContainer = textContainerRef.current;
-               setDataUrl(detail.data);
-               setCreditsUrl(credits.data.cast);
-               setSimilarUrl(similar.data.results);
-               setSocialUrl(social.data);
+                const textContainer = textContainerRef.current;
+                setDataUrl(detail.data);
+                setCreditsUrl(credits.data.cast);
+                setSimilarUrl(similar.data.results);
+                setSocialUrl(social.data);
 
 
-               console.log(params.id)
+                console.log(params.id)
                 // 이미지
-               const images = await movieApi.seasonImg(params.id);
-               setImagesUrl(images.data);
+                const images = await movieApi.seasonImg(params.id);
+                setImagesUrl(images.data);
 
-               const videos = await movieApi.seasonVideo(496243);
-               setVideoUrl(videos.data.results)
+                const videos = await movieApi.seasonVideo(params.id);
+                setVideoUrl(videos.data.results)
 
-               console.log(videos.data.results)
+                console.log(videos.data.results)
 
-               // tv 시리즈
-               if (params.type === 'tv') {
-                   const seasons = await movieApi.seasons(params.id, dataUrl?.number_of_seasons);
-                   setSeasonUrl(seasons.data);
-                   const episode = await movieApi.episode(params.id, dataUrl?.number_of_seasons, '1');
-                   setEpisodeUrl(episode.data);
-               }
+                // tv 시리즈
+                if (params.type === 'tv') {
+                    const seasons = await movieApi.seasons(params.id, dataUrl?.number_of_seasons);
+                    setSeasonUrl(seasons.data);
+                    const episode = await movieApi.episode(params.id, dataUrl?.number_of_seasons, '1');
+                    setEpisodeUrl(episode.data);
+                }
 
-               // 영화 상세설명
-               const handleResize = () => {
-                   if (textContainer) {
-                       setIsOverflowed(textContainer.scrollHeight > textContainer.clientHeight);
-                   }
-               };
-               handleResize();
-               window.addEventListener('resize', handleResize);
-               return () => {
-                   window.removeEventListener('resize', handleResize);
-               };
+                // 영화 상세설명
+                const handleResize = () => {
+                    if (textContainer) {
+                        setIsOverflowed(textContainer.scrollHeight > textContainer.clientHeight);
+                    }
+                };
+                handleResize();
+                window.addEventListener('resize', handleResize);
+                return () => {
+                    window.removeEventListener('resize', handleResize);
+                };
 
-           } catch(error) {
-               console.error('Eroror', error);
-           }
+            } catch (error) {
+                console.error('Eroror', error);
+            }
         }
+
         Api();
     }, [textContainerRef.current, params.id]);
 
     /* 소셜 */
     const socialMedia = [
-        { name : '페이스북', url : 'http://www.facebook.com', class : "facebook", link : `${socialUrl?.facebook_id}`},
-        { name : '트위터', url : 'http://www.twitter.com', class : "twitter", link : `${socialUrl?.twitter_id}`},
-        { name : '인스타그램', url : 'http://www.instagram.com', class : "instagram", link : `${socialUrl?.instagram_id}`}
+        {name: '페이스북', url: 'http://www.facebook.com', class: "facebook", link: `${socialUrl?.facebook_id}`},
+        {name: '트위터', url: 'http://www.twitter.com', class: "twitter", link: `${socialUrl?.twitter_id}`},
+        {name: '인스타그램', url: 'http://www.instagram.com', class: "instagram", link: `${socialUrl?.instagram_id}`}
     ]
 
     /* 등장인물 */
-    const creditsArray = creditsUrl ? creditsUrl.slice(0,5) : [];
+    const creditsArray = creditsUrl ? creditsUrl.slice(0, 5) : [];
 
     /* 비슷한 작품 */
-    const similarArray = similarUrl ? similarUrl.slice(0,5): [];
+    const similarArray = similarUrl ? similarUrl.slice(0, 5) : [];
 
     return (
         <div>
-            <section className="detail_container" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${dataUrl?.backdrop_path})` }}>
+            <section className="detail_container"
+                     style={{backgroundImage: `url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${dataUrl?.backdrop_path})`}}>
                 <div className="detail_info">
                     <h1>{dataUrl?.title || dataUrl?.name}</h1>
                     <div className="meta">
@@ -195,7 +193,8 @@ export default function ItemDetail() {
                         {socialMedia.map(item => {
                             return item.link !== "null" ? (
                                 <li>
-                                    <a href={`${item.url}/${item.link}`} className={`${item.class}`} target="_blank" key={item.id}>
+                                    <a href={`${item.url}/${item.link}`} className={`${item.class}`} target="_blank"
+                                       key={item.id}>
                                         <span className="blind">{item.name}</span>
                                     </a>
                                 </li>
@@ -203,7 +202,8 @@ export default function ItemDetail() {
                         })}
                     </ul>
                     <picture>
-                        <img src={`https://image.tmdb.org/t/p/w500/${dataUrl?.poster_path}`} alt="Movie Poster" loading="lazy"/>
+                        <img src={`https://image.tmdb.org/t/p/w500/${dataUrl?.poster_path}`} alt="Movie Poster"
+                             loading="lazy"/>
                     </picture>
                 </div>
             </section>
@@ -222,7 +222,8 @@ export default function ItemDetail() {
                             </div>
                             <div className="season_box">
                                 <Link to={`/series/${params.id}/episode`} className="season_main">
-                                    <img src={`https://image.tmdb.org/t/p/w500/${seasonUrl.poster_path}`} alt="" loading="lazy"/>
+                                    <img src={`https://image.tmdb.org/t/p/w500/${seasonUrl.poster_path}`} alt=""
+                                         loading="lazy"/>
                                 </Link>
                                 <List type={params.type} list={seasonList} class={"season_list"}></List>
                             </div>
@@ -234,44 +235,45 @@ export default function ItemDetail() {
                     <h2>미디어</h2>
                     <ul className="type_list">
                         <li>
-                            <button type="button" className={mediaType === 'backdrops' ? 'active' : ''} onClick={() => mediaTab('backdrops')} >동영상</button>
+                            <button type="button" className={mediaType === 'video' ? 'active' : ''}
+                                    onClick={() => mediaTab('video')}>동영상
+                            </button>
                         </li>
                         <li>
-                            <button type="button" className={mediaType === 'backdrops' ? 'active' : ''} onClick={() => mediaTab('backdrops')}>배경</button>
+                            <button type="button" className={mediaType === 'backdrops' ? 'active' : ''}
+                                    onClick={() => mediaTab('backdrops')}>배경
+                            </button>
                         </li>
                         <li>
-                            <button type="button" className={mediaType === 'posters' ? 'active' : ''} onClick={() => mediaTab('posters')}>포스터</button>
+                            <button type="button" className={mediaType === 'posters' ? 'active' : ''}
+                                    onClick={() => mediaTab('posters')}>포스터
+                            </button>
                         </li>
                     </ul>
                 </div>
 
 
                 <Swiper slidesPerView={'auto'} className="media_slide">
-                    {
-                        videoUrl && videoUrl.map((item, index) => (
-                            mediaType === 'video' && (
+                    {mediaType === 'video' &&
+                        (videoUrl &&
+                            videoUrl.map((item, index) => (
                                 <SwiperSlide key={index} className="bg_card" onClick={() => videoBox(item)}>
-                                    <span>{item.key}</span>
                                     <img src={`https://i.ytimg.com/vi/${item.key}/hqdefault.jpg`} alt=""/>
-                                    <iframe src={`https://www.youtube.com/embed/${item.key}`} frameBorder="0" allowFullScreen></iframe>
                                 </SwiperSlide>
-                            )
-                        ))
-                    }
+                            )))}
+
+                    {(mediaType === 'backdrops' || mediaType === 'posters') &&
+                        (imagesUrl[mediaType] &&
+                            imagesUrl[mediaType].map((item, index) => (
+                                <SwiperSlide key={index} className="bg_card">
+                                    <button type="button" className="media_link" onClick={() => imgMore(item)}>
+                                        <img src={`https://image.tmdb.org/t/p/w500${item.file_path}`} alt="Movie Poster"
+                                             loading="lazy"/>
+                                    </button>
+                                </SwiperSlide>
+                            )))}
                 </Swiper>
 
-
-                <Swiper slidesPerView={'auto'} className="media_slide">
-                    {
-                        imagesUrl && imagesUrl[mediaType].map((item, index) => (
-                            <SwiperSlide key={index} className="bg_card">
-                                <button type="button" className="media_link" onClick={() => imgMore(item)}>
-                                    <img src={`https://image.tmdb.org/t/p/w500${item.file_path}`} alt="Movie Poster" loading="lazy" />
-                                </button>
-                            </SwiperSlide>
-                        ))
-                    }
-                </Swiper>
 
                 <div className="title"><h2>비슷한 작품</h2></div>
                 <List type={params.type} list={similarArray} class={"item_list"}></List>
@@ -280,7 +282,8 @@ export default function ItemDetail() {
                     videoOpen && (
                         <div className="video_modal">
                             <div className="inner">
-                                <iframe src={`https://www.youtube.com/embed/${youtubeUrl}`} frameBorder="0" allowFullScreen></iframe>
+                                <iframe src={`https://www.youtube.com/embed/${youtubeUrl}`} frameBorder="0"
+                                        allowFullScreen></iframe>
                                 <button type="button" className="modal_close" onClick={videoModalClose}>
                                     <span className="blind">닫기</span>
                                 </button>
@@ -293,7 +296,8 @@ export default function ItemDetail() {
                         <div className="video_modal">
                             <div className="inner">
                                 <a href={`https://www.themoviedb.org/t/p/original${videoLink}`} target="_blank">
-                                    <img src={`https://image.tmdb.org/t/p/w500${videoLink}`} alt="Movie Poster" loading="lazy" />
+                                    <img src={`https://image.tmdb.org/t/p/w500${videoLink}`} alt="Movie Poster"
+                                         loading="lazy"/>
                                 </a>
                                 <p className="img_txt">이미지를 클릭하여 원본 ({videoSize.width}X{videoSize.height})을 확인 해보세요!</p>
                                 <button type="button" className="modal_close" onClick={imgModalClose}>
