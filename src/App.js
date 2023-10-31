85133213327d3e124fe35a8ec1608588eedd6c17
nujinging <nujinging@naver.com>
2023-10-31 11:04:43 +0900

    깃헙 배포세팅

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.scss';
-import { Route, Routes } from 'react-router-dom';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 import Main from "./components/Main";
 import ItemDetail from "./components/ItemDetail";
 import Header from "./components/Header";
@@ -8,13 +8,12 @@ import Genre from "./components/Genre";
 import Search from "./components/Search";
 import PersonDetail from "./components/PersonDetail";
 import SeriesDetail from "./components/SeriesDetail";
-import {useState, useEffect} from "react";
 import NotFound from "./components/NotFound";
 
 export default function App() {
 
   return (
-      <div>
+      <HashRouter basename="/">
           <Header></Header>
           <Routes>
               <Route path='/' element={<Main />} ></Route>
@@ -25,6 +24,6 @@ export default function App() {
               <Route path='/series/:id/episode' element={<SeriesDetail />} ></Route>
               <Route path="*" element={<NotFound />} ></Route>
           </Routes>
-      </div>
+      </HashRouter>
   );
 }
