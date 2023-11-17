3eaba7f77f3bb014520862e2d60f417a8e669920
nujinging <nujinging@naver.com>
2023-11-17 17:55:25 +0900

    메인 슬라이드 autoplay추가

--- src/components/HomeSlide.js
+++ src/components/HomeSlide.js
@@ -1,6 +1,6 @@
 import './../App.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import {  Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import "swiper/css";
 import "swiper/css/navigation";
 import {useNavigate} from "react-router-dom";
@@ -26,7 +26,7 @@ export default function HomeSlide(props) {
     };
 
     return (
-        <Swiper pagination={pagination} navigation={true} loop={true} modules={[ Pagination, Navigation]} className="home_banner">
+        <Swiper pagination={pagination} navigation={true} loop={true} autoplay={{ delay: 2500, disableOnInteraction: false, }} modules={[ Autoplay, Pagination, Navigation]} className="home_banner">
             {slideList.map(item => (
                 <SwiperSlide className="banner" key={item.id} style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${item.backdrop_path})` }} onClick={() => pageLink(item.id)}>
                     <div className="banner_txt">
